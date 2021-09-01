c28c7c0ea659acb5c21dbb2ae353046978dfa321
BANCOARBI\alexandre.arphoenix <alexandre.arphoenix@bancoarbi.com.br>
2021-09-01 13:46:14 -0300

    inclusão do projeto no git

---
+++ netbanking.RemessaArquivo.API/Models/Remessa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Netbanking.RemessaArquivo.API.Models
+{
+    public class RemessaItem
+    {
+        public long Id { get; set; }
+        public string arquivo { get; set; }
+    }
+}
---
+++ netbanking.RemessaArquivo.API/Models/RemessaContext .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Netbanking.RemessaArquivo.API.Models
+{
+    public class RemessaContext : DbContext
+    {
+        public RemessaContext(DbContextOptions<RemessaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RemessaItem> RemessaItems { get; set; }
+    }
+}
