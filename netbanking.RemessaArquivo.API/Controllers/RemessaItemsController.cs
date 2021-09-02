b8d43b1e1546a06d1d6e9a8f9a07a8f73421431a
BANCOARBI\alexandre.arphoenix <alexandre.arphoenix@bancoarbi.com.br>
2021-09-02 10:31:19 -0300

    Ajustes finos

--- netbanking.RemessaArquivo.API/Controllers/RemessaItemsController.cs
+++ netbanking.RemessaArquivo.API/Controllers/RemessaItemsController.cs
@@ -44,7 +44,6 @@ namespace Netbanking.RemessaArquivo.API.Controllers
         }
 
         // PUT: api/RemessaItems/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutRemessaItem(long id, RemessaItem remessaItem)
         {
@@ -75,7 +74,6 @@ namespace Netbanking.RemessaArquivo.API.Controllers
         }
 
         // POST: api/RemessaItems
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<RemessaItem>> PostRemessaItem(RemessaItem remessaItem)
         {
